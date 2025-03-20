defdcfd6196b4c8663427f2ac1a41829ba95c179
sergio-garita <andres90890@gmail.com>
2025-03-19 20:08:11 -0600

    Add files via upload

---
+++ Caso_2/src/caso_2/Caso_2.java
@@ -0,0 +1,31 @@
+
+package caso_2;
+
+import javax.swing.JOptionPane;
+
+public class Caso_2 {
+
+    public static void main(String[] args) {
+        
+
+        
+
+            
+            
+    
+            
+            
+            
+        }
+    }
+    
+    
+            
+    
+    
+    
+    
+
+
+
+
